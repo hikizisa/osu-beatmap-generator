3f6df2d1384623180df6efba539ce67325bdba96
Zisa <hikizisa@users.noreply.github.com>
2018-09-25 14:42:50 +0900

    getosulist
    get .osu files in dir

--- venv/Read Map List.py
+++ venv/Read Map List.py
@@ -1,2 +1,23 @@
 import numpy as np
+import os
 
+#returns list of files in dir
+def getosulist(dir):
+    files = os.listdir(dir)
+    listfile = []
+    for file in files:
+        dirfile = os.path.join(dir, file)
+        if os.path.isdir(dirfile):
+            indirosu = getosulist(dirfile)
+            listfile = listfile + dirfile
+        else:
+            ext = os.path.splitext(full_filename)[-1]
+            if ext == '.osu':
+                listfile.append(os.path.join(dir,file))
+    return listfile
+
+#get path of .py file
+curdir = os.path.dirname(os.path.realpath(__file__))
+
+#relative path to song folder
+reldir = "/songs"
