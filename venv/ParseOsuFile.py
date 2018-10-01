2e02003035ccc8803aba80234630e1e672eb6f7f
Zisa <hikizisa@users.noreply.github.com>
2018-10-01 20:10:43 +0900

    parseosu
    base

--- venv/ParseOsuFile.py
+++ venv/ParseOsuFile.py
@@ -1,6 +1,20 @@
 import numpy as np
 import os, configparser
+import osuT as o
 
+def makeDataset(listBeatmap):
+    pass
 
+def sortDifficulty(listBeatmap):
+    pass
+
+def makeTrainData(beatmap):
+    pass
+
+def loadSongData(musicfile):
+    pass
+
+def loadBeatmapData(osufile):
+    pass
 
 listBeatmap = np.load(savePath+'.npy')
